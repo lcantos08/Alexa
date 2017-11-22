e44b4cf01ca3766fbcf085552b2ad6da893b1b09
Lui Cantos <lcantos08@gmail.com>
2017-11-22 22:04:41 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const SKILL_ID = 'amzn1.ask.skill.6c4d29d2-3a3a-4b90-8274-78846c5d5864';
 
 const WELCOME_MSG = ''.concat(
-    'Welcome to Body Tech Lemery. ',
-    'Please ask me about the status of the daily sales, staff commissions and expenses. ',
-    'For example, say how much is the total sales today or what are the expenses today?'
+    'Hi, Welcome to Body Tech Lemery. ',
+    'Please ask me about the status of the daily sales, staff commissions, and expenses. ',
+    'For example, simply say how much is the total sales today, or what are the expenses today?'
 );
 const REPROMPT_TEXT = 'Ask me where your package is by saying Where is order 12345?';
 
@@ -63,7 +63,7 @@ alexaApp.intent("OrderTrackingIntent", {
         "utterances": [
             "for status on order number {OrderId}",
             "about {OrderId}",
-            "staff commision"
+            "daily sales {OrderId}"
         ]
     },
     function(request, response) {
