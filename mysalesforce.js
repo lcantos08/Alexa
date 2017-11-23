2d888f5755eea458eb85d09ba331660e75588dbd
Lui Cantos <lcantos08@gmail.com>
2017-11-23 19:09:08 +1000

    Update mysalesforce.js

--- mysalesforce.js
+++ mysalesforce.js
@@ -92,6 +92,7 @@ function getDailySummary(ddate){
                 speechOutput = `There are ${result.length} ${pluralize("services",result.length)} so far. `;
                 speechOutput += '<break strength="x-strong"/>Total amount is ';
                 for (let i = 0; i < result.length; i++) {
+                    console.log('result[i] : '+result[i]);
                     amount+= parseFloat(result[i].price);
                 }
                 
